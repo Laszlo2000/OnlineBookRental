1789c48b880f025ab6f02ecb75a9d5218ce61420
Szabó Dávid <david030719@gmail.com>
2024-11-11 21:54:30 +0100

    base project

---
+++ backend/src/test/java/project/BookRental/BookRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package project.BookRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
