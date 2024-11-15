0a0fabfddac8e541dd8a994369fec8e16f78a6dc
Szabó Dávid <david030719@gmail.com>
2024-11-15 10:43:51 +0100

    feat: add Book table for storing book information

---
+++ backend/src/main/java/project/BookRental/entity/BookEntity.java
@@ -0,0 +1,95 @@
+package project.BookRental.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class BookEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "author")
+    private String author;
+    @Column(name = "genre")
+    private String genre;
+    @Column(name = "isAvailable")
+    private boolean isAvailable;
+    @Column(name = "isbn")
+    private int isbn;
+
+    public BookEntity() {
+    }
+
+    public BookEntity(long id, String title, String author, String genre, boolean isAvailable, int isbn) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.isAvailable = isAvailable;
+        this.isbn = isbn;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BookEntity that = (BookEntity) o;
+        return id == that.id && isAvailable == that.isAvailable && isbn == that.isbn && Objects.equals(title, that.title) && Objects.equals(author, that.author) && Objects.equals(genre, that.genre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, author, genre, isAvailable, isbn);
+    }
+}
