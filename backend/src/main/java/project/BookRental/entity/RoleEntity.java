aff5ec50916e9bfd1482a84f70ead2fd524cb9ce
Szabó Dávid <david030719@gmail.com>
2024-11-15 10:45:42 +0100

    feat: add Role table for storing user roles

---
+++ backend/src/main/java/project/BookRental/entity/RoleEntity.java
@@ -0,0 +1,52 @@
+package project.BookRental.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class RoleEntity {
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "role")
+    private String role;
+
+    public RoleEntity() {
+    }
+
+    public RoleEntity(long id, String role) {
+        this.id = id;
+        this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoleEntity that = (RoleEntity) o;
+        return id == that.id && Objects.equals(role, that.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, role);
+    }
+}
