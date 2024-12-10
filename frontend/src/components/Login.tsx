70f6ffb964a1d5253c1ff195f31749b753d6ed9b
Ilonczai László <laszlo2000@mailbox.unideb.hu>
2024-12-10 16:02:49 +0100

    Style Login

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -34,8 +34,8 @@ const Login: React.FC = () => {
 
   return (
     <div className="flex justify-center items-center h-screen w-screen bg-zinc-850 fixed top-0 left-0">
-      <form onSubmit={handleLogin} className="flex flex-col gap-4 w-72 bg-zinc-700 p-6 rounded-lg shadow-lg">
-        <h1 className="text-center text-white mb-5 text-3xl">Login</h1>
+      <form onSubmit={handleLogin} className="flex flex-col gap-4 w-72 bg-[#80AF81] p-6 rounded-xl shadow-lg">
+        <h1 className="text-center text-black mb-5 text-3xl">Login</h1>
         <input
           type="text"
           name="username"
@@ -43,7 +43,7 @@ const Login: React.FC = () => {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           required
-          className="p-2 border border-gray-300 rounded-md bg-black text-white"
+          className="p-2 border border-gray-300 rounded-md w-full text-black"
         />
         <input
           type="password"
@@ -52,14 +52,14 @@ const Login: React.FC = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
-          className="p-2 border border-gray-300 rounded-md bg-black text-white"
+          className="p-2 border border-gray-300 rounded-md w-full text-black"
         />
-        <button type="submit" className="p-2 bg-blue-600 hover:bg-blue-700 text-white rounded-md cursor-pointer">
+        <button type="submit" className="p-2 bg-[#2A3B2D] hover:bg-[#202B21]  text-white rounded-md cursor-pointer">
           Login
         </button>
-        <p className="text-center">
+        <p className="text-center text-black">
           Does not have an account? <br />{" "}
-          <a href="/register" className="text-blue-400">
+          <a href="/register" className="text-blue-700">
             Register here!
           </a>
         </p>
